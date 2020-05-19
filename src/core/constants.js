b48f3e348b8815ef22c73713941da98f4f408275
mrodriguezarias <14845585+mrodriguezarias@users.noreply.github.com>
2020-05-19 06:51:40 -0300

    z

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,6 +1,6 @@
 export const WEBSOCKET_URL = "http://localhost:3750"
 export const API_URL = "http://localhost:3750/api"
 export const ZOOM_URL =
-  "https://us04web.zoom.us/j/8281256817?pwd=eWVickxzUmxjcEpyeTdTZjJOUkdUdz09"
-export const ZOOM_PMI = "8281256817"
+  "https://zoom.us/j/6266337820?pwd=L1hPa1BKNy8zczBNVzRCT1V1N0pBZz09"
+export const ZOOM_PMI = "6266337820"
 export const ZOOM_PASS = "fiesta1905"
